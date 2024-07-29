4c00c4ef57a1febf85d9b95c06011c9d4cca34ff
psahir <psahir289@gmail.com>
2024-07-29 19:29:08 +0400

    comment added

--- src/components/modelPopup/ModelPopUp.jsx
+++ src/components/modelPopup/ModelPopUp.jsx
@@ -10,6 +10,10 @@ const ModelPopUp = (props) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     console.log("🚀 ~ setTimeout ~ modelData?.return_url:", modelData?.return_url)
 
+
+    // Also add the notify url and data to give data to user.
+
+
     useEffect(() => {
         const timer = setTimeout(() => {
             // Open the URL in a new tab
@@ -21,16 +25,16 @@ const ModelPopUp = (props) => {
     }, [modelData]);
 
     return (
-        <Modal open={true} width={900} className='font-serif' footer={false}>
+        <Modal open={paymentModel} width={900} className='font-serif' footer={false}>
             <div className='flex justify-center mt-10'>
                 <div className='bg-green-800 w-32 h-32 rounded-[100%] flex' >
-                    {modelData?.status === "SUCCESS" && <div className='flex ps-5 mt-4'> <CheckIcon /></div>}
+                    {modelData?.status === "Success" && <div className='flex ps-5 mt-4'> <CheckIcon /></div>}
                     {modelData?.status === "Not Found" && <span className='flex items-center ps-2'><ClockIcon /></span>}
                 </div>
 
             </div>
             <div className='flex justify-center mt-5 text-3xl  font-bold'>
-                <p>{modelData?.status === "SUCCESS" && "Payment completed"}{modelData?.status === "Not Found" && "UTR Submitted!!!"}{modelData?.status === "DUPLICATE" && "Duplicate UTR found!!!"}</p>
+                <p>{modelData?.status === "Success" && "Payment completed"}{modelData?.status === "Not Found" && "UTR Submitted!!!"}{modelData?.status === "DUPLICATE" && "Duplicate UTR found!!!"}</p>
             </div>
             {modelData?.status === "Not Found" && <div className='text-center '>Your points will be credited in few minutes</div>}
             {modelData?.status === "DUPLICATE" && <div className='text-center text-red-600 '>Please contact to customer services for further info.</div>}
@@ -39,11 +43,11 @@ const ModelPopUp = (props) => {
             <div className='mt-16'>
                 <div className='flex justify-between'>
                     <p className='text-xl   '>Amount</p>
-                    <p className='text-xl  font-bold '>{modelData?.status === "SUCCESS" ? modelData?.amount : "--"}{modelData?.status === "Not Found" && modelData?.amount}</p>
+                    <p className='text-xl  font-bold '>{modelData?.status === "Success" ? modelData?.amount : modelData?.status === "Not Found" ? modelData?.amount : "--"}</p>
                 </div>
                 <div className='flex justify-between mt-6'>
                     <p className='text-xl  '>UTR No.</p>
-                    <p className='text-xl  font-bold '>{modelData?.status === "SUCCESS" ? modelData?.utr : "--"}</p>
+                    <p className='text-xl  font-bold '>{modelData?.status === "Success" ? modelData?.utr : "--"}</p>
                 </div>
                 <div className='flex justify-between  mt-6'>
                     <p className='text-xl  '>Transaction Id.</p>
