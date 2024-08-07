f720bb53daf33a6166eaa66f0ce72acc3929e6b2
psahir <psahir289@gmail.com>
2024-08-07 22:04:22 +0400

    env changes

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-// const BASE_URL = "http://localhost:9000";
-const BASE_URL = "http://localhost:8080/a2x/v1";
+const BASE_URL = `${import.meta.env.VITE_API_BACKEND_URL}`;
 
 const http = axios.create({
     baseURL: BASE_URL,
