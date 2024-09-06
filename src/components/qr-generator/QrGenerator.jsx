900a6d14046b4215fdf54eb2ec053db06c07979a
psahir <psahir289@gmail.com>
2024-09-06 18:55:40 +0530

    qr size change

--- src/components/qr-generator/QrGenerator.jsx
+++ src/components/qr-generator/QrGenerator.jsx
@@ -5,8 +5,7 @@ const QrGenerator = ({upi_id, amount, code,ac_name}) => {
     const payload = `upi://pay?pa=${upi_id}&pn=${ac_name}&am=${amount}&cu=INR&tn=${code}`
     return (
         <div>
-           {/* <QRCode value={`upi://pay?pa=${upiId}&pn=xi-cash&am=${amount}&cu=usd`} /> */}
-           <QRCode value={payload} />
+           <QRCode value={payload} size={200}/>
 
         </div>
     )
