69b9d14ee55a2e3f63af61c73444519fb2607852
Shadow <shadow77278837@gmail.com>
2024-10-21 23:07:09 +0400

    Update Bank.jsx

--- src/components/banktransfer/Bank.jsx
+++ src/components/banktransfer/Bank.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { MdOutlineTimer } from "react-icons/md";
 import { Divider } from 'antd';
-import { RiFileCopyFill } from "react-icons/ri";
+import { MdCopyAll } from "react-icons/md";
 import { copyToClipboard } from '../../utils';
 import "./Bank.css"
 
@@ -13,23 +13,18 @@ const Bank = ({ ac_name, ac_no, bank_name, ifsc, amount , name , }) => {
       <div className="bank-container">
         <p className="text">Payment Time Left</p>
         <div className="right-area">
-          <MdOutlineTimer size={20} color="white" />
-          <p className="timer-text" id="bank-timer">
-            00:10:00
-          </p>
+          <MdOutlineTimer size={20} color='black' />
+          <p className='timer-text' id='bank-timer'>00:10:00</p>
         </div>
       </div>
       <div className="section-container">
-        <div className="section-to">
-          <span>
-            Scan & Pay the exact amount only
-            <br /> (upto lakh ) submit UTR
-          </span>
+        <div className="section-to text-sm">
+          <span><b>Please scan and pay the exact amount, then share the UTR for confirmation</b></span>
         </div>
         <Divider />
         <div className="amount-container">
-          <p className="amount-text">Transfer Amount</p>
-          <p className="price">Rs{amount}/-</p>
+          <p className='amount-text text-2xl'>Amount :</p>&nbsp;&nbsp;&nbsp;&nbsp;
+          <p className='price text-2xl'>₹{amount}</p>
         </div>
         <Divider />
         <div className="details">
