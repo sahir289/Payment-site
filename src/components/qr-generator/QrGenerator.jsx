792f2037b70583a8e718c31d5276cce6f117f30a
sudhakartajalfahidi <sudhakar.tajalfahidi@gmail.com>
2024-10-21 21:52:19 +0400

    Revert "Payment page UI Changes"
    This reverts commit 84eac0b7479f64930e962642b8faa14615449029.

--- src/components/qr-generator/QrGenerator.jsx
+++ src/components/qr-generator/QrGenerator.jsx
@@ -5,7 +5,8 @@ const QrGenerator = ({upi_id, amount, code,ac_name}) => {
     const payload = `upi://pay?pa=${upi_id}&pn=${ac_name}&am=${amount}&cu=INR&tn=${code}`
     return (
         <div>
-           <QRCode value={payload} size={180}/>
+           <QRCode value={payload} size={200}/>
+
         </div>
     )
 }
