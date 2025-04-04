13d3eb8a8a0324574e63c66cc11dab912643db75
Shadow <shadow77278837@gmail.com>
2025-04-04 16:39:09 +0400

    Handle Multi Click Issue for Direct DUPLICATE TXN

--- src/screens/transactions/Transactions.jsx
+++ src/screens/transactions/Transactions.jsx
@@ -189,6 +189,7 @@ const Transactions = () => {
   };
 
   const handleUtrNumber = async (data) => {
+    if (processing) return;
     const updateData = {
       usrSubmittedUtr: data?.utrNumber?.trim(),
       code: transactionsInformation?.code,
@@ -280,6 +281,7 @@ const Transactions = () => {
   };
 
   const handleImgSubmit = async (data) => {
+    if (processing) return;
     const token = params.token;
     const fileData = data.img[0];
     if (!fileData) {
